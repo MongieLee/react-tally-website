d781b9ed51f4348e260ea1f8d8e1c46b4f897b2b
MongieLee <mongielee@163.com>
2020-06-22 23:14:49 +0800

    deploy

---
+++ precache-manifest.72961a3e2807da3d5a78d326f3fa2866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c47ddfe706bad759f8a2c66b9d6151",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ce1257fe488baf37c50",
+    "url": "./static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "21333fa68aad8d54a415",
+    "url": "./static/css/main.61d73996.chunk.css"
+  },
+  {
+    "revision": "4ce1257fe488baf37c50",
+    "url": "./static/js/2.d1492ca5.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "./static/js/2.d1492ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21333fa68aad8d54a415",
+    "url": "./static/js/main.3f3b4be6.chunk.js"
+  },
+  {
+    "revision": "b9e33cbcd65bcea71bbd",
+    "url": "./static/js/runtime-main.20c67fe1.js"
+  }
+]);
